fafb06602c316714998bf50b021f66607d667f94
Taylor Peet <repeet13@gmail.com>
2016-03-01 09:27:04 -0800

    fixing a few things, adding some things

--- Renamer/renamer.py
+++ Renamer/renamer.py
@@ -4,9 +4,15 @@ cwd = os.getcwd()
 onlyfiles = [f for f in os.listdir(cwd) if os.path.isfile(os.path.join(cwd, f))]
  
 sep = ' - '
+season = 1
 offset = 0
- 
+## TODO pad zeros based on length of onlyfiles
+
 for f in onlyfiles:
-  parts = f.split(sep)
-  nnewf = '.'.join([parts[0], str(int(parts[1])-offset), parts[2]])
-  # shutil.move(f, newf)
+    print '------'
+    print f
+    if any(i in f for i in sep):
+        parts = f.split(sep)
+        newf = '.'.join([parts[0], 'S'+str(season)+'E'+str(int(parts[1])-offset), parts[3]])
+        print newf
+#       shutil.move(f, newf)
